c30c9301fcece4b728b071c9413293d820cd95e8
Masahiro <53962817+masahiro96848@users.noreply.github.com>
2022-06-23 08:57:35 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -69,15 +69,38 @@
 //   return str;
 // }
 
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
-// アロ-関数
-const func2 = (str) => str;
-console.log(func2("func2です"));
+// // アロ-関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
+// const func3 = (num1, num2) => num1 + num2;
 
-console.log(func3(10, 20));
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "test",
+//   age: 28
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+const myProfile = ["test", 28];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}です。`;
+console.log(message4);
