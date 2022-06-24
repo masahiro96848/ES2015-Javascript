715690046c03ef6b83775d7594f6fbdd569a5db5
Masahiro <53962817+masahiro96848@users.noreply.github.com>
2022-06-24 21:58:49 +0900

    まとめ

--- src/index.js
+++ src/index.js
@@ -194,7 +194,7 @@
 //   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
 // console.log(formattedNum);
 
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
-};
-console.log(checkSum(50, 6));
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 6));
